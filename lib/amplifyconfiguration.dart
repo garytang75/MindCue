3eb43da5058f034df9ee10f24289caefd573284f
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2021-02-28 23:20:40 -0500

    configure auth with new account

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,19 +1,6 @@
 const amplifyconfig = ''' {
     "UserAgent": "aws-amplify-cli/2.0",
     "Version": "1.0",
-    "analytics": {
-        "plugins": {
-            "awsPinpointAnalyticsPlugin": {
-                "pinpointAnalytics": {
-                    "appId": "a5bc35b6dca84081a25d67847cf2a224",
-                    "region": "us-east-1"
-                },
-                "pinpointTargeting": {
-                    "region": "us-east-1"
-                }
-            }
-        }
-    },
     "auth": {
         "plugins": {
             "awsCognitoAuthPlugin": {
@@ -22,36 +9,17 @@ const amplifyconfig = ''' {
                 "IdentityManager": {
                     "Default": {}
                 },
-                "CredentialsProvider": {
-                    "CognitoIdentity": {
-                        "Default": {
-                            "PoolId": "us-east-1:a2ae7850-e76f-486e-ac70-75ea8674899d",
-                            "Region": "us-east-1"
-                        }
-                    }
-                },
                 "CognitoUserPool": {
                     "Default": {
-                        "PoolId": "us-east-1_WfzKjg9NX",
-                        "AppClientId": "71fagk6hngs0slb1fgo3a27elc",
-                        "AppClientSecret": "1a2hfmcru8i5jtgtuareg661nddlgq0tp6qhkol5uqa3p33th641",
+                        "PoolId": "us-east-1_N4sv3wjm4",
+                        "AppClientId": "55b40r95lsek2doea9sr018rm3",
+                        "AppClientSecret":"1ouuvihmel0uf46gf2pegsolih08veug8t4d6hl6tct4f7e4qekq",
                         "Region": "us-east-1"
                     }
                 },
                 "Auth": {
                     "Default": {
                         "authenticationFlowType": "USER_SRP_AUTH"
-                    }
-                },
-                "PinpointAnalytics": {
-                    "Default": {
-                        "AppId": "a5bc35b6dca84081a25d67847cf2a224",
-                        "Region": "us-east-1"
-                    }
-                },
-                "PinpointTargeting": {
-                    "Default": {
-                        "Region": "us-east-1"
                     }
                 }
             }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter_login/flutter_login.dart';
-import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+// import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
 import 'package:amplify_core/amplify_core.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 
@@ -38,7 +38,7 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     // Add Pinpoint and Cognito Plugins
-    Amplify.addPlugin(AmplifyAnalyticsPinpoint());
+    // Amplify.addPlugin(AmplifyAnalyticsPinpoint());
     Amplify.addPlugin(AmplifyAuthCognito());
 
     // Once Plugins are added, configure Amplify
@@ -109,171 +109,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_login/flutter_login.dart';
-// import 'package:rflutter_alert/rflutter_alert.dart';
-// import 'package:amplify_flutter/amplify.dart';
-// import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
-// import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-// import 'amplifyconfiguration.dart';
-//
-// void main() {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> {
-//   bool _amplifyConfigured = false;
-// //
-//   // Instantiate Amplify
-//   //Amplify amplifyInstance = Amplify();
-//
-//   // controllers for text input
-//   final emailController = TextEditingController();
-//   final passwordController = TextEditingController();
-//
-//   bool isSignUpComplete = false;
-//   bool isSignedIn = false;
-// //
-//   @override
-//   void initState() {
-//     super.initState();
-//     _configureAmplify();
-//   }
-//   //
-//   @override
-//   void dispose() {
-//     // Clean up the controller when the widget is removed from the
-//     // widget tree.
-//     emailController.dispose();
-//     passwordController.dispose();
-//
-//     super.dispose();
-//   }
-//   //
-//   void _configureAmplify() async {
-//     if (!mounted) return;
-//
-//     // Add Pinpoint and Cognito Plugins
-//     Amplify.addPlugin(AmplifyAnalyticsPinpoint());
-//     Amplify.addPlugin(AmplifyAuthCognito());
-//
-//
-//     // Once Plugins are added, configure Amplify
-//     // Note: Amplify can only be configured once.
-//     try {
-//       await Amplify.configure(amplifyconfig);
-//     } on AmplifyAlreadyConfiguredException {
-//       print("Amplify was already configured. Was the app restarted?");
-//     }
-//     try {
-//       setState(() {
-//         _amplifyConfigured = true;
-//       });
-//     } catch (e) {
-//       print(e);
-//     }
-//
-//   }
-//
-//   // Send an event to Pinpoint
-//   void _recordEvent() async {
-//     AnalyticsEvent event = AnalyticsEvent('test');
-//     event.properties.addBoolProperty('boolKey', true);
-//     event.properties.addDoubleProperty('doubleKey', 10.0);
-//     event.properties.addIntProperty('intKey', 10);
-//     event.properties.addStringProperty('stringKey', 'stringValue');
-//     Amplify.Analytics.recordEvent(event: event);
-//   }
-//
-//   Future<String> _registerUser(LoginData data) async {
-//     try {
-//       Map<String, String> userAttributes = {
-//         "email": emailController.text,
-//       };
-//       SignUpResult res = await Amplify.Auth.signUp(
-//           username: data.name,
-//           password: data.password,
-//           options: CognitoSignUpOptions(userAttributes: userAttributes)
-//       );
-//       setState(() {
-//         isSignUpComplete = res.isSignUpComplete;
-//         if (!isSignUpComplete) {
-//             // needs email confirmation
-//             print("Not yet Complete, needs email confirmation");
-//
-//           } else {
-//             // signup was ok. do login
-//             print("Complete");
-//           }
-//         // print("Sign up: " + (isSignUpComplete ? "Complete" : "Not Complete"));
-//         // print(isSignUpComplete);
-//       });
-//     } on AuthException catch (e) {
-//       print(e);
-//       return "Register Error: " + e.toString();
-//     }
-// //confirm signup
-//     try {
-//       print('test');
-//       SignUpResult res = await Amplify.Auth.confirmSignUp(
-//           username: data.name,
-//           confirmationCode: '12345'
-//       );
-//       setState(() {
-//         isSignUpComplete = res.isSignUpComplete;
-//
-//         // print("Sign up: " + (isSignUpComplete ? "Complete" : "Not Complete"));
-//         // print(isSignUpComplete);
-//       });
-//     } on AuthException catch (e) {
-//       print(e.message);
-//     }
-//
-//   }
-//
-//   Future<String> _signIn(LoginData data) async {
-//     try {
-//       SignInResult res = await Amplify.Auth.signIn(
-//         username: data.name,
-//         password: data.password,
-//       );
-//       setState(() {
-//         isSignedIn = res.isSignedIn;
-//       });
-//
-//       if (isSignedIn)
-//         // Alert(context: context, type: AlertType.success, title: "Login Success")
-//         //     .show();
-//         return 'Logged in!';
-//     } on AuthException catch (e) {
-//       print(e.message);
-//       // Alert(context: context, type: AlertType.error, title: "Login Failed")
-//       //     .show();
-//       return 'Log In Error: ' + e.toString();
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//           body: SafeArea(
-//               child: FlutterLogin(
-//                   logo: "assets/logo.png",
-//                   onLogin: _signIn,
-//                   onSignup: _registerUser,
-//                   onRecoverPassword: (_) => null,
-//                   title: 'Welcome!',),
-//
-//             ),
-//           ),
-//     );
-//
-//   }
-// }

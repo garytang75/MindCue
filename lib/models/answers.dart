bf18687feb5b0936ebab3daf2575b19d0eb9abe0
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2021-04-01 11:34:13 -0400

    add questions, style answers

--- lib/models/answers.dart
+++ lib/models/answers.dart
@@ -7,12 +7,22 @@ class Answers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: double.infinity,
-        child: RaisedButton(
-          color: Colors.indigo,
-          textColor: Colors.white,
+        width: 210,
+        // child: IconButton(onPressed: selectHandler
+        //   , icon: Image.asset('assets/icon/mood-sad.png', width: 35, height: 35),
+        // ),
+        child: ElevatedButton(
           child: Text(answerText),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(new Color(0xFFFDBC59)),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  )
+              )
+          ),
           onPressed: selectHandler,
-        ));
+        )
+    );
   }
 }
--- lib/page/questionnaires_page.dart
+++ lib/page/questionnaires_page.dart
@@ -13,28 +13,91 @@ class _QuestionnairesState extends State<Questionnaires> {
     {
       'questionText': 'How are you feeling today?',
       'answer': [
-        {'text': 'Excellent', 'score': 10},
-        {'text': 'Good', 'score': 7},
-        {'text': 'Bad', 'score': 4},
-        {'text': 'Horrible', 'score': 1},
+        {'text': 'Excellent', 'score': 4},
+        {'text': 'Good', 'score': 3},
+        {'text': 'Meh', 'score': 2},
+        {'text': 'Bad', 'score': 1},
       ],
     },
     {
-      'questionText': 'How tired are you? ',
+      'questionText': 'Tell me about your energy level? ',
       'answer': [
-        {'text': 'Not at all', 'score': 10},
-        {'text': 'A little', 'score': 7},
-        {'text': 'Very Tired', 'score': 3},
-        {'text': 'Exhausted', 'score': 1}
+        {'text': 'Full of energy', 'score': 4},
+        {'text': 'Average', 'score': 3},
+        {'text': 'Low', 'score': 2},
+        {'text': 'Burnt out', 'score': 1}
       ],
     },
     {
-      'questionText': 'Do you feel productive today?',
+      'questionText': 'Tell me about your concentration?',
       'answer': [
-        {'text': 'I accomplished a lot', 'score': 10},
-        {'text': 'I accomplished a little', 'score': 6},
-        {'text': 'I accomplished very little', 'score': 4},
-        {'text': 'I am a couch potato', 'score': 1}
+        {'text': 'Always ease to focus', 'score': 4},
+        {'text': 'Usually ease ', 'score': 3},
+        {'text': 'Sometimes hard ', 'score': 2},
+        {'text': 'Very hard to focus', 'score': 1}
+      ],
+    },
+    {
+      'questionText': 'Tell me about your appetite?',
+      'answer': [
+        {'text': 'Excellent', 'score': 4},
+        {'text': 'Good', 'score': 3},
+        {'text': 'Meh', 'score': 2},
+        {'text': 'Bad', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'Do you feel nervous, anxious, or on edge?',
+      'answer': [
+        {'text': 'Not at all', 'score': 4},
+        {'text': 'Rare', 'score': 3},
+        {'text': 'Sometimes', 'score': 2},
+        {'text': 'Always', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'How often do you feel being so restless that it is hard to sit still?',
+      'answer': [
+        {'text': 'Not at all', 'score': 4},
+        {'text': 'Rare', 'score': 3},
+        {'text': 'Sometimes', 'score': 2},
+        {'text': 'Always', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'How often do you feel being afraid, as if something awful might happen?',
+      'answer': [
+        {'text': 'Not at all', 'score': 4},
+        {'text': 'Rare', 'score': 3},
+        {'text': 'Sometimes', 'score': 2},
+        {'text': 'Always', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'Are you able to control the important things in your life?',
+      'answer': [
+        {'text': 'Always', 'score': 4},
+        {'text': 'Most of the time', 'score': 3},
+        {'text': 'Sometimes out of control', 'score': 2},
+        {'text': 'Hard to control', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'Are things going your way?',
+      'answer': [
+        {'text': 'Always', 'score': 4},
+        {'text': 'Sometimes', 'score': 3},
+        {'text': 'Rare', 'score': 2},
+        {'text': 'Never', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'How do you feel about overcoming difficulties?',
+      'answer': [
+        {'text': 'Easy to overcome', 'score': 4},
+        {'text': 'Usually ease', 'score': 3},
+        {'text': 'Sometimes hard', 'score': 2},
+        {'text': 'Very hard', 'score': 1}
       ],
     },
   ];
@@ -72,8 +135,10 @@ class _QuestionnairesState extends State<Questionnaires> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        accentColor: Colors.white,
+        appBarTheme: AppBarTheme(
+        color: Color(0xFFCEE2EE),
+        ),
+        accentColor: Colors.black,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
